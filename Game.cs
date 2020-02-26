0e860398e1fb9f26b3c3cc87a7ae117135004968
Michał Salamon <ms055452@student.ath.edu.pl>
2020-02-26 23:26:40 +0100

    Initial commit

---
+++ Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParseHTML
+{
+    class Game
+    {
+        public string AwayTeam { get;}
+        public string HomeTeam { get;}
+        public DateTime Date { get;}
+        public bool Finished { get;}
+        public int AwayScore { get;}
+        public int HomeScore { get;}
+
+        public Game(string awayTeam, string homeTeam, DateTime date, int awayScore, int homeScore) : this(awayTeam, homeTeam, date)
+        {
+            AwayScore = awayScore;
+            HomeScore = homeScore;
+            Finished = true;
+        }
+
+        public Game(string awayTeam, string homeTeam, DateTime date)
+        {
+            AwayTeam = awayTeam;
+            HomeTeam = homeTeam;
+            Date = date;
+            Finished = false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace ParseHTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient webClient = new WebClient();
+            string page = webClient.DownloadString("https://www.landofbasketball.com/results/2019_2020_scores_full.htm");
+
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(page);
+
+            List<List<string>> table = doc.DocumentNode.SelectSingleNode("//table[@class='color-alt max-1']")
+                        .Descendants("tr")
+                        .Skip(1)                        
+                        .Select(tr => tr.Elements("td")
+                        .Select(td => td.InnerText.Trim().Replace(System.Environment.NewLine,""))
+                        .ToList())
+                        .ToList();
+            foreach (var item in table)
+            {
+                foreach (var item2 in item)
+                {
+                    var temp = item2.Trim().Split("\t");
+                    foreach (var bub in temp)
+                    {
+                        if (!string.IsNullOrWhiteSpace(bub))
+                        Console.WriteLine("item:" + bub.Replace("@ ",""));
+                    }
+                    Console.WriteLine("Pauza");
+                    //Console.WriteLine("item: "+item2);
+                }
+                Console.WriteLine();
+            }
+            //foreach (var list1 in table)
+            //{
+            //    foreach (var list2 in list1)
+            //    {
+            //        var item = list2.Trim().Split("\t").Where(;
+            //        item=item.Where(i=>!string.IsNullOrWhiteSpace(i))
+            //    }
+            //}
+            var s = DateTime.Parse("Feb 1, 2020");
+            Console.WriteLine(s.ToString());
+        }
+        
+    }
+}

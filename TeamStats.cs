b55679fcb58a459f875f10a69e82015bf331ebd3
Michał Salamon <ms055452@student.ath.edu.pl>
2020-02-27 14:07:17 +0100

    add winning percentage

---
+++ TeamStats.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ParseHTML
+{
+    public class TeamStats
+    {
+        List<Game> gamesList;
+        List<Game> gamesPlayed;
+        List<Game> gamesToBePlayed;
+        public TeamStats(List<Game> _gamesList)
+        {
+            gamesList = _gamesList;
+            gamesPlayed = _gamesList.Where(g => g.Finished == true).ToList();
+            gamesToBePlayed = _gamesList.Where(g => g.Finished == false).ToList();
+        }
+        public double WinningPercentage(string teamName)
+        {
+            //int played = gamesList.Where()
+            double won = gamesPlayed.Where(t =>
+           (t.AwayTeam == teamName && t.AwayScore > t.HomeScore)
+           ||
+           (t.HomeTeam == teamName && t.HomeScore > t.AwayScore))
+               .Count();
+
+            double played =
+            gamesPlayed.Where(t => t.AwayTeam == teamName || t.HomeTeam == teamName)
+            .Count();
+            //Console.WriteLine("Won: {0}, Played: {1}", won, played);
+            return won / played;
+        }
+        //public double 
+    }
+}
